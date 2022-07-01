88460879f7ec366a6ad5c14d33e54f2ed007be29
tigzk <onlinepeopleoficial@gmail.com>
2022-07-01 13:11:02 -0300

    1.0 Release

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </body>
 </html>
 <?php
-$code = "'from complemento import nome, autor, descricao; print(\"<h1>\" + nome + \" by \" + autor + \"</h1><br><br>\" + descricao)'";
+$code = "'from complemento import nome, autor, descricao; print(\"<h1>\" + nome + \" \" + versao + \"</h1><br><br>\" + descricao)'";
 $cmd = "python -c " . $code;
 $output = shell_exec($cmd);
 echo $output;
