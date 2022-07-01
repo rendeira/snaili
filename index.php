ede0a5a2650f946d6fd7b2b6282e9ec986693fec
tigzk <onlinepeopleoficial@gmail.com>
2022-07-01 12:15:54 -0300

    teste

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 </body>
 </html>
 <?php
+echo 'Loading info, please wait'
 $code = "'print(nome + \" by \" + autor + \"\\n\" + descricao)'";
 $cmd = "python complemento.py -c " . $code;
 $result = exec($cmd);
